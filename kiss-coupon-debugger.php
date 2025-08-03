38bda4af49643821369cb52246f7c4eb7c91ae96
noelsaw1 <noel@neochro.me>
2025-08-03 15:00:02 -0700

    1st commit

--- kiss-coupon-debugger.php
+++ kiss-coupon-debugger.php
@@ -10,30 +10,6 @@
  * Text Domain: wc-sc-debugger
  * Domain Path: /languages
  *
- * Changelog
- *
- * #### 1.2.6 - 2025-08-03
- * - Tweak: Added deep logging for `woocommerce_add_to_cart_validation` filter and any generated WC notices to better capture plugin conflicts.
- *
- * #### 1.2.5 - 2025-08-03
- * - Feature: Added an environment versions panel to the top of the debugger page.
- *
- * #### 1.2.4 - 2025-08-03
- * - Feature: Added the plugin version number to admin page titles.
- *
- * #### 1.2.3 - 2025-08-03
- * - Tweak: Added explicit logging for the success or failure of the `add_to_cart` function.
- *
- * #### 1.2.2 - 2025-08-03
- * - Tweak: Added deep logging for cart contents and product prices to debug zero-total issue.
- *
- * #### 1.2.1 - 2025-08-03
- * - Tweak: Added more detailed logging during coupon test to show pre-coupon total, coupon details, and discount amount.
- *
- * #### 1.2.0 - 2025-08-03
- * - Fix: PHP fatal error by ensuring WC notices are correctly handled during the test simulation.
- * - Tweak: Improved session handling during the coupon test.
- *
  * @package WC_SC_Debugger
  */
 
